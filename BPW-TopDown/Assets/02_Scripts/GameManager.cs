cd1dd155ccce862c8984096b3f4c2277d9844d46
Watenk <Ivar.nuij@gmail.com>
2022-11-14 15:33:58 +0100

    Added Basic character Controller

--- BPW-TopDown/Assets/02_Scripts/GameManager.cs
+++ BPW-TopDown/Assets/02_Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     void Start()
     {
-        
+
     }
 
     void Update()
--- BPW-TopDown/Assets/02_Scripts/Player.cs
+++ BPW-TopDown/Assets/02_Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float jumpForce;
+    public float jumpAccelerationTime;
+
+    private Rigidbody2D rb;
+    public bool touchingGround = false;
+    private bool jump;
+    private float jumpAccelerationTimer;
+    private float horizontalInput;
+
+    public void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        //Inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown("space") && touchingGround == true)
+        {
+            jump = true;
+            touchingGround = false;
+            jumpAccelerationTimer = jumpAccelerationTime;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        //Movement
+        rb.velocity = new Vector2(horizontalInput * speed, 0); 
+
+        if (jump == true)
+        {
+            rb.AddForce(transform.up * (jumpForce * 10) * jumpAccelerationTimer);
+
+            jumpAccelerationTimer -= 1;
+            if (jumpAccelerationTimer < 1) { jump = false; }
+        }
     }
 }
